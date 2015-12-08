fef1f549692862c058014ba7deaae8d13e4d6523
James Molson <molson@lal.in2p3.fr>
2015-12-08 20:30:27 +0100

    Cleaning up OneMeter.

--- SixtrackCollimationTools/OneMetre.h
+++ SixtrackCollimationTools/OneMetre.h
@@ -1,28 +1,28 @@
-/*
- * OneMetre.h (SR, May 2004)
- *
- * Definition of one object that contains the aaperture
- * definition along one metre of the LHC sequence.
- * The aperture of the all sequence can be defined with 
- * a vector of OneMetre objects.
- *
- * The aperture is defined by specifying an arbitrary
- * number of 'Aperture' objects within the OneMetre, at 
- * a given longitudinal position. Only the fractional
- * part of the position ( p-floor(p) ) is taken into 
- * accont. The idea is that the position along the LHC
- * sequence will be given by the index 'i' that number the 
- * various OneMetres in the vector that defines the all
- * sequence (i=0:27000).
- *
- * In order to be able to calculate the aperture in any
- * position along the considered metre, and not only at
- * the positions where the aperture is defined, the
- * member funtion "GetAperture(double p)" can be used
- * It calculate the aperture as interpolation of the
- * point where the aperture as been defined.
- *
- */
+/**
+* OneMetre.h (SR, May 2004)
+*
+* Definition of one object that contains the aaperture
+* definition along one metre of the LHC sequence.
+* The aperture of the all sequence can be defined with 
+* a vector of OneMetre objects.
+*
+* The aperture is defined by specifying an arbitrary
+* number of 'Aperture' objects within the OneMetre, at 
+* a given longitudinal position. Only the fractional
+* part of the position ( p-floor(p) ) is taken into 
+* accont. The idea is that the position along the LHC
+* sequence will be given by the index 'i' that number the 
+* various OneMetres in the vector that defines the all
+* sequence (i=0:27000).
+*
+* In order to be able to calculate the aperture in any
+* position along the considered metre, and not only at
+* the positions where the aperture is defined, the
+* member funtion "GetAperture(double p)" can be used
+* It calculate the aperture as interpolation of the
+* point where the aperture as been defined.
+*
+*/
 
 #include "Aperture.h"
 
@@ -46,32 +46,75 @@ using namespace std;
 #ifndef OneMetre_h
 #define OneMetre_h 1
 
-class OneMetre {
+class OneMetre
+{
 private:
-  //
-  vector<double> Pos;       // Position where aperture is defined.
-  vector<Aperture> Apert;   // Corresponding apertures.
-  vector<double> Pos_ex;    // Extended position and aperture vector with additional
-  vector<Aperture> Apert_ex;// definitions at 0.0 and 1.0, for aperture interpolation.
-                            // Defined once for all the fist time that 'GetAperture' is called
-  //  vector<double> Apert_vec; // Aperture in vector format - useful later
-  Aperture Atmp;
-  //  vector<int> ind; // Index of positions with defined aperture
-  vector<double> Atmp_vec; // Temporary vector for various internal uses
+
+	/**
+	* Position where aperture is defined.
+	*/
+	vector<double> Pos;
+
+	/**
+	* Corresponding apertures.
+	*/
+	vector<Aperture> Apert;
+
+	/**
+	* Extended position and aperture vector with additional
+	*/
+	vector<double> Pos_ex;
+
+	/**
+	* definitions at 0.0 and 1.0, for aperture interpolation.
+	*/
+	vector<Aperture> Apert_ex;
+
+	/**	
+	* Aperture in vector format - useful later
+	*/
+	//	vector<double> Apert_vec;
+
+	/**
+	* Defined once for all the fist time that 'GetAperture' is called
+	*/
+	Aperture Atmp;
+
+	/**
+	* Index of positions with defined aperture
+	*/
+	//	vector<int> ind;
+
+	/**
+	* Temporary vector for various internal uses
+	*/
+	vector<double> Atmp_vec;
 
 public:
-  OneMetre();
-  ~OneMetre();
-  void DefineAperture(double p, double a1, double a2, double a3, double a4);
-  void DefineAperture(double p, Aperture Ap);
-  void DefineAperture(double p, vector<double> A4);
-  //
-  void GetApertDef(vector<double> *Position, vector<Aperture> *ThisApert);
-  //
-  Aperture GetAperture(double p); // Get aperture at a given position
-  //
-  void empty();
-  void status();
+
+	OneMetre();
+
+	~OneMetre();
+
+	void DefineAperture(double p, double a1, double a2, double a3, double a4);
+
+	void DefineAperture(double p, Aperture Ap);
+
+	void DefineAperture(double p, vector<double> A4);
+
+	void GetApertDef(vector<double> *Position, vector<Aperture> *ThisApert);
+
+	/**
+	* Get aperture at a given position.
+	* @parm[in] p The position to get the Aperture.
+	* @return An Aperture at the requested position.
+	*/
+	Aperture GetAperture(double p);
+
+	void empty();
+
+	void status();
 };
 
 #endif
+
