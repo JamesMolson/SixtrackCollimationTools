fde9563243b61e6b2663fb99ec3891ca83b76c08
James Molson <molson@lal.in2p3.fr>
2015-12-09 14:24:47 +0100

    Cleanup for OneMetreAlign.

--- SixtrackCollimationTools/OneMetreAlign.h
+++ SixtrackCollimationTools/OneMetreAlign.h
@@ -1,7 +1,7 @@
-/*
- * OneMetreAlign.h (SR, June 2004)
- *
- */
+/**
+* OneMetreAlign.h (SR, June 2004)
+*
+*/
 
 #include "Aperture.h"
 
@@ -25,43 +25,86 @@ using namespace std;
 #ifndef OneMetreAlign_h
 #define OneMetreAlign_h 1
 
-class OneMetreAlign {
+class OneMetreAlign
+{
 private:
-  vector<double> Pos;       // Position where aperture is defined.
-  vector<Aperture> Apert;   // Corresponding apertures.
-  vector<double> DxAlign,   // Corresponding alignment statements.
-    DyAlign;
-  vector<double> Pos_ex;    // Extended position and aperture vector with additional
-  vector<Aperture> Apert_ex;// definitions at 0.0 and 1.0, for aperture interpolation.
-  vector<double> DxAlign_ex,// Defined once for all the fist time that 'GetAperture' is called
-    DyAlign_ex;
-                            
-  //  vector<double> Apert_vec; // Aperture in vector format - useful later
-  Aperture Atmp;
-  //  vector<int> ind; // Index of positions with defined aperture
-  vector<double> Atmp_vec; // Temporary vector for various internal uses
-  double DxA, DyA;
+
+	/**
+	* Position where aperture is defined.
+	*/
+	vector<double> Pos;
+
+	/**
+	* Corresponding apertures.
+	*/
+	vector<Aperture> Apert;
+
+	/**
+	* Corresponding alignment statements.
+	*/
+	vector<double> DxAlign,	DyAlign;
+
+	/**
+	* Extended position and aperture vector with additional
+	*/
+	vector<double> Pos_ex;
+
+	/**
+	* definitions at 0.0 and 1.0, for aperture interpolation.
+	*/
+	vector<Aperture> Apert_ex;
+
+	/**
+	* Defined once for all the fist time that 'GetAperture' is called
+	*/
+	vector<double> DxAlign_ex, DyAlign_ex;
+
+	/**
+	* Aperture in vector format - useful later
+	*/
+	//vector<double> Apert_vec; 
+
+	Aperture Atmp;
+
+	/**
+	* Index of positions with defined aperture
+	*/
+	//vector<int> ind;
+
+	/**
+	* Temporary vector for various internal uses
+	*/
+	vector<double> Atmp_vec;
+
+	double DxA, DyA;
 
 public:
-  OneMetreAlign();
-  ~OneMetreAlign();
-  // If alignment is not defined, put it to zero!
-  void DefineAperture(double p, double a1, double a2, double a3, double a4);
-  void DefineAperture(double p, Aperture Ap);
-  void DefineAperture(double p, vector<double> A4);
-  //
-  void DefineApertureAlign(double p, double a1, double a2, double a3, double a4, double Dx, double Dy);
-  void DefineApertureAlign(double p, Aperture Ap, double Dx, double Dy);
-  void DefineApertureAlign(double p, vector<double> A4, double Dx, double Dy);
-  //
-  void GetApertDef(vector<double> *Position, vector<Aperture> *ThisApert);
-  //
-  Aperture GetAperture(double p); // Get aperture at a given position
-  double GetAlignX(double p);
-  double GetAlignY(double p);
-  //
-  void empty();
-  void status();
+
+	OneMetreAlign();
+	~OneMetreAlign();
+
+	// If alignment is not defined, put it to zero!
+	void DefineAperture(double p, double a1, double a2, double a3, double a4);
+	void DefineAperture(double p, Aperture Ap);
+	void DefineAperture(double p, vector<double> A4);
+
+	void DefineApertureAlign(double p, double a1, double a2, double a3, double a4, double Dx, double Dy);
+	void DefineApertureAlign(double p, Aperture Ap, double Dx, double Dy);
+	void DefineApertureAlign(double p, vector<double> A4, double Dx, double Dy);
+
+	void GetApertDef(vector<double> *Position, vector<Aperture> *ThisApert);
+
+	/**
+	* Get aperture at a given position
+	*/
+	Aperture GetAperture(double p);
+
+	double GetAlignX(double p);
+	double GetAlignY(double p);
+
+	void empty();
+	void status();
 };
 
 #endif
+
