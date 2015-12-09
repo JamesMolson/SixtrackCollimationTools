f3aa75721f1afd24bde74c859504010fea4e959e
James Molson <molson@lal.in2p3.fr>
2015-12-09 11:43:12 +0100

    Cleanup for Survey.

--- SixtrackCollimationTools/Survey.h
+++ SixtrackCollimationTools/Survey.h
@@ -1,14 +1,6 @@
 /*
- * fix creator and distructor!
- *
- *
- *
- *
- *
- *
- *
- *
- */
+* fix creator and distructor!
+*/
 
 #include <string>
 #include <sstream>
@@ -30,30 +22,34 @@
 
 using namespace std;
 
-class Survey {
- private:
-  vector<double> S, Xsurvey;
-  vector<double> Xcrossing, Ycrossing;
-  vector<double> XPcrossing, YPcrossing;
-  vector<double> COx, COy;
+class Survey
+{
+private:
 
-  public: 
-  Survey();
-  ~Survey();
-  //
-  void LoadLHC(string in);
-  void LoadLHC_Crossing(string in);
-  void LoadLHC_Crossing_XP(string in);
-  double GetSurvey(double pos);
-  double GetCrossX(double pos);
-  double GetCrossY(double pos);
-  double GetCrossXP(double pos);
-  double GetCrossYP(double pos);
-  void Clear();
-  //
-  void LoadCO(string in);
-  double GetCOx(double pos);
-  double GetCOy(double pos);
+	vector<double> S, Xsurvey;
+	vector<double> Xcrossing, Ycrossing;
+	vector<double> XPcrossing, YPcrossing;
+	vector<double> COx, COy;
+
+public:
+
+	Survey();
+	~Survey();
+
+	void LoadLHC(string in);
+	void LoadLHC_Crossing(string in);
+	void LoadLHC_Crossing_XP(string in);
+	double GetSurvey(double pos);
+	double GetCrossX(double pos);
+	double GetCrossY(double pos);
+	double GetCrossXP(double pos);
+	double GetCrossYP(double pos);
+	void Clear();
+
+	void LoadCO(string in);
+	double GetCOx(double pos);
+	double GetCOy(double pos);
 };
 
 #endif
+
